ef899fbe136308ad6bae6d0a4dafe94032e4a7cf
Vladyslav Halushkin <v.halushkin@gmail.com>
2019-04-11 15:39:19 +0300

    Updates

---
+++ precache-manifest.4bae8aa72742be47f3638f188549cd57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0214e8ba4d13138031fc170b0a871d09",
+    "url": "/GlobalLogic-TestTask/static/media/bg.0214e8ba.jpg"
+  },
+  {
+    "revision": "7fc77eb05bb6350a29ef",
+    "url": "/GlobalLogic-TestTask/static/js/runtime~main.a26349a2.js"
+  },
+  {
+    "revision": "dcb48595f9420c36cf0b",
+    "url": "/GlobalLogic-TestTask/static/js/main.2306ec89.chunk.js"
+  },
+  {
+    "revision": "e4723eb62012227ef88c",
+    "url": "/GlobalLogic-TestTask/static/js/2.eba529f8.chunk.js"
+  },
+  {
+    "revision": "dcb48595f9420c36cf0b",
+    "url": "/GlobalLogic-TestTask/static/css/main.3ebdb83b.chunk.css"
+  },
+  {
+    "revision": "8eb698181ca2fe87a8b3e4851dafefb0",
+    "url": "/GlobalLogic-TestTask/index.html"
+  }
+];
